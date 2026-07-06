9a7c28caf41d935578d28d934b5de1af1152dad8
Inventrip <inventrip@gmail.com>
2026-07-06 09:52:54 +0530

    sidebar address chnages

--- src/layouts/headers/Menu/Sidebar.tsx
+++ src/layouts/headers/Menu/Sidebar.tsx
@@ -19,7 +19,7 @@ const Sidebar = ({ sidebar, setSidebar }: SidebarProps) => {
             <div className="offCanvas__side-info mb-30">
                <div className="contact-list mb-30">
                   <h4>Office Address</h4>
-                  <p>123/Shimla  <br /> Himachal Pradesh</p>
+                  <p>Karsog District Mandi Himachal Pradesh 175011</p>
                </div>
                <div className="contact-list mb-30">
                   <h4>Phone Number</h4>
